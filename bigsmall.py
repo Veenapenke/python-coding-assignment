ffaff118800c8a1846d15ce8b2d3b3a9226bbac3
Veenapenke <171228352+Veenapenke@users.noreply.github.com>
2025-03-07 08:03:34 +0530

    Delete bigsmall.py

--- bigsmall.py
+++
@@ -1,9 +0,0 @@
-a = int(input("Enter first number: "))
-b = int(input("Enter second number: "))
-
-if a < b:
-    print("Small number:", a)
-    print("Large number:", b)
-else:
-    print("Smaller number:", b)
-    print("Larger number:", a)
